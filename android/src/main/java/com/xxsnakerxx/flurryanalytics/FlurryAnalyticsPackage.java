aebf9b73f8d1f01bbae896a42802b25c2ddd30d1
Irwing <irwing85@gmail.com>
2017-11-07 08:37:32 -0600

    Make compatible with native 0.50

--- android/src/main/java/com/xxsnakerxx/flurryanalytics/FlurryAnalyticsPackage.java
+++ android/src/main/java/com/xxsnakerxx/flurryanalytics/FlurryAnalyticsPackage.java
@@ -25,8 +25,12 @@ public class FlurryAnalyticsPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+    
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 }
